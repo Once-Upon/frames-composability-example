2ecfd6ae78f22457fc824675dac7f28e9186dcc3
Paul Cowgill <pauldavidcowgill@gmail.com>
2024-01-29 15:40:48 -0600

    Use different frames

--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,19 @@ import path from "path";
 
 const filePath = path.join(__dirname, "static/home.html");
 
-const pokeFrameFilePath = path.join(__dirname, "static/PokeFrame.html");
+const onceUponFilePath = path.join(__dirname, "static/OnceUpon.html");
 const yoinkFilePath = path.join(__dirname, "static/Yoink.html");
 const tenetFilePath = path.join(__dirname, "static/Tenet.html");
-
-// TODO: Pick fourth frame
-const todoFilePath = path.join(__dirname, "static/Todo.html");
+const degenMachineFilePath = path.join(__dirname, "static/DegenMachine.html");
 
 const home = fs.readFileSync(filePath, { encoding: "utf-8" });
 
-const pokeFrame = fs.readFileSync(pokeFrameFilePath, { encoding: "utf-8" });
+const onceUpon = fs.readFileSync(onceUponFilePath, { encoding: "utf-8" });
 const yoink = fs.readFileSync(yoinkFilePath, { encoding: "utf-8" });
 const tenet = fs.readFileSync(tenetFilePath, { encoding: "utf-8" });
-const todo = fs.readFileSync(todoFilePath, { encoding: "utf-8" });
+const degenMachine = fs.readFileSync(degenMachineFilePath, {
+  encoding: "utf-8",
+});
 
 app.use(express.json());
 
@@ -34,13 +34,13 @@ app.post("/", (req, res) => {
   const buttonIndex = body?.untrustedData?.buttonIndex;
   console.log("buttonIndex", buttonIndex);
   if (req.body.untrustedData.buttonIndex === 1) {
-    res.send(pokeFrame);
+    res.send(onceUpon);
   } else if (req.body.untrustedData.buttonIndex === 2) {
     res.send(yoink);
   } else if (req.body.untrustedData.buttonIndex === 3) {
     res.send(tenet);
   } else if (req.body.untrustedData.buttonIndex === 4) {
-    res.send(todo);
+    res.send(degenMachine);
   } else {
     // Respond with an error
     res.send("Error");
